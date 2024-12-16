35639cb3e565da14d8e028dea219c38948d92611
Kostadin Penev <kostadin.penev@nevexis.com>
2024-12-16 15:35:59 +0200

    Day15 pt1

--- src/current/Day15.kt
+++ src/current/Day15.kt
@@ -1,4 +1,6 @@
 import utils.Utils
+import java.util.*
+import kotlin.collections.HashMap
 
 class Day15 {
 
@@ -6,29 +8,59 @@ class Day15 {
 
 val pair = getStartInfo()
 val start: Pair<Int, Int>? = pair.first
-val matrix = pair.second
+val end: Pair<Int, Int>? = pair.second
+val matrix = pair.third
 var shortest: Long = Long.MAX_VALUE;
 fun main() {
 
-    if(start != null) {
-        for(d in Utils.Direction.entries) {
-            val visited = mutableMapOf<Pair<Int, Int>, Long>()
-            val nextStep = Pair(start.first + d.pair.first, start.second + d.pair.second)
-            visited[start] = 1
-            if (nextStep.first in matrix.indices && nextStep.second in matrix.indices && matrix[nextStep.first][nextStep.second] == '.') {
-                val turn = if(d==Utils.Direction.RIGHT) 0L else 1L
-                val currentShortest = dfs(nextStep, 2, turn, d, visited)
-                if(currentShortest != null) {
-                    val calculated =currentShortest.first + 1000*currentShortest.second
-                    if (calculated < shortest) {
-                        shortest = calculated
-                    }
+    if (start != null && end != null) {
+        val queue = PriorityQueue<Triple<Pair<Int, Int>, Utils.Direction, Int>>(compareBy { it.third })
+        queue.add(Triple(start, Utils.Direction.RIGHT, 0))
+
+        val seen = HashMap<Pair<Pair<Int, Int>, Utils.Direction>, Int>()
+        while (queue.isNotEmpty()) {
+            val (p, d, s) = queue.poll()
+
+            if (p == end) {
+                if (s.toLong() < sho) {
+                    sho = s.toLong()
                 }
+                continue;
+            }
+
+            if (seen[p to d] != null && seen[p to d]!! < s) continue
+            seen[p to d] = s
+            val x = p.first + d.pair.first
+            val y = p.second + d.pair.second
+
+            if (x < matrix.size && y < matrix[x].size && matrix[x][y] != '#') {
+                queue.add(Triple(Pair(x, y), d, s + 1))
+            }
+            val perpendicular = Utils.Direction.getPerpendicular(d)
+            perpendicular.forEach {
+                queue.add(Triple(p, it, s + 1000))
             }
         }
+
+//        for (d in Utils.Direction.entries) {
+//            val visited = mutableMapOf<Pair<Int, Int>, Long>()
+//            val nextStep = Pair(start.first + d.pair.first, start.second + d.pair.second)
+//            visited[start] = 1
+//            if (nextStep.first in matrix.indices && nextStep.second in matrix.indices && matrix[nextStep.first][nextStep.second] == '.') {
+//                val turn = if (d == Utils.Direction.RIGHT) 0L else 1L
+//                val currentShortest = dfs(nextStep, 2, turn, d, visited)
+//                if (currentShortest != null) {
+//                    val calculated = currentShortest.first + 1000 * currentShortest.second
+//                    if (calculated < shortest) {
+//                        shortest = calculated
+//                    }
+//                }
+//            }
+//        }
         println(sho)
     }
 }
+
 var sho = Long.MAX_VALUE
 private fun dfs(
     current: Pair<Int, Int>,
@@ -44,12 +76,12 @@ private fun dfs(
     if (currentChar == 'E') {
         return Pair(count, turns)
     }
-    if(sho < count + turns*1000) {
+    if (sho < count + turns * 1000) {
         return null
     }
     val contains = visited[current]
     if (contains != null) {
-        if(contains > count) {
+        if (contains > count) {
             visited.remove(current)
         } else {
             return null
@@ -57,7 +89,7 @@ private fun dfs(
     }
     visited[current] = count
 
-    if(currentChar == '#') {
+    if (currentChar == '#') {
         return null;
     } //123424
     val oppositeDir = Utils.Direction.getOpposite(currentDirection);
@@ -76,21 +108,24 @@ private fun dfs(
                 } else currentTurns
                 val dfs = dfs(nextStep, count + 1, nextTurns, direction, visited)
 
-                if(dfs != null) {
-                    if(dfs.first + dfs.second*1000 < sho) {
-                        sho =  dfs.first + dfs.second*1000
+                if (dfs != null) {
+                    if (dfs.first + dfs.second * 1000 < sho) {
+                        sho = dfs.first + dfs.second * 1000
                     }
                 }
             }
-        } catch (_: Exception) {return null}
+        } catch (_: Exception) {
+            return null
+        }
     }
 
     return null
 }
 
 
-private fun getStartInfo(): Pair<Pair<Int, Int>?, Array<CharArray>> {
+private fun getStartInfo(): Triple<Pair<Int, Int>?, Pair<Int, Int>?, Array<CharArray>> {
     var start: Pair<Int, Int>? = null
+    var end: Pair<Int, Int>? = null
     var currentLineIndex = 0
     val matrix = mutableListOf<List<Char>>()
     readFile("Day15.txt")?.forEachLine {
@@ -99,8 +134,12 @@ private fun getStartInfo(): Pair<Pair<Int, Int>?, Array<CharArray>> {
         if (indexOf != -1) {
             start = Pair(currentLineIndex, indexOf)
         }
+        val indexOfE = it.indexOf('E')
+        if (indexOfE != -1) {
+            end = Pair(currentLineIndex, indexOfE)
+        }
         currentLineIndex++;
 
     }
-    return Pair(start, matrix.map{ it.toCharArray() }.toTypedArray())
+    return Triple(start, end, matrix.map { it.toCharArray() }.toTypedArray())
 }
